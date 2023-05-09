c454229b8e9d21381b0a1e30f91aa7aa7afb30c2
LucianoPiassi <98159156+LucianoPiassi@users.noreply.github.com>
2023-05-09 12:39:12 -0300

    Add files via upload

---
+++ Desafio_Potencia.rb
@@ -0,0 +1,18 @@
+#LUCIANO PIASSI
+#************************************************************************
+#Criar um array Vazio
+#Usuário insere 3 Numeros
+#Mostra resultado dos 3 Numeros elevados a Terceira Potência
+#************************************************************************
+
+#Declaração de Variaveis
+Numeros=[] #declara vetor/array vazio
+
+#Entrada de dados
+
+puts "Entre com numero 1:"
+Numeros[0] = gets
+
+#TODO:  Crie as condições necessárias para calcular o número
+#médio de coxinhas
+#puts ("%.2f" % (a.to_f/b.to_f))
